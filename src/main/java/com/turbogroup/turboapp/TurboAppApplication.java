40227041c7e5608dcb235bc6d5368b4cf12a15a5
Łukasz Wysmoliński <lukasz.wysmolinski@gmail.com>
2020-03-03 23:41:42 +0100

    Initial commit after connection to turbodb

---
+++ src/main/java/com/turbogroup/turboapp/TurboAppApplication.java
@@ -0,0 +1,13 @@
+package com.turbogroup.turboapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurboAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurboAppApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/turbogroup/turboapp/repository/Turbocharger.java
@@ -0,0 +1,7 @@
+package com.turbogroup.turboapp.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface Turbocharger {
+}
